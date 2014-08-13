4cd24a5965b374609f924fe6737c5ee72b0b7163
Albert Martinez <albertm@fullsail.edu>
2014-08-13 08:29:43 -0700

    added folders for screencast ex + age example

---
+++ Expressions/ageExample/js/script.js
@@ -0,0 +1,6 @@
+//////////Expressions - Age Example
+//age formula: Current Year - Birth Year
+
+var yearBorn = 1988; //stored my birthyear in the variable yearBorn
+var age = 2014 - yearBorn - 1; //store result of age formula in variable age
+console.log(age); //output: 25
