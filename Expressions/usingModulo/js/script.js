8b824383bc9f18a5ec460eeed8e8f6c692f7578d
Albert Martinez <albertm@fullsail.edu>
2014-08-13 08:50:29 -0700

    added Modulo code from screencast

--- Expressions/usingModulo/js/script.js
+++ Expressions/usingModulo/js/script.js
@@ -1,2 +1,10 @@
+//Expressions - Modulo operator: finding remainder
+
+var remainder = 32 % 10;
+console.log(remainder);
+
+//great way to check if # is even/odd
+var eveOddCheck = 32 % 2;//even #'s have 0 remainder when divided by 2
+console.log(remainder);//output: 0
 
 
