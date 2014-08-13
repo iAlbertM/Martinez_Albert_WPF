f75a81965e93f3f872c10904e28ce5961c2c8fab
Albert Martinez <albertm@fullsail.edu>
2014-08-13 07:57:54 -0700

    added code for "Basic Expressions" screencast

---
+++ Expressions/js/script.js
@@ -0,0 +1,9 @@
+////////// Basic Expressions
+
+var a = 2;
+console.log(a + 3); //Output: 5. does not change value of a
+
+
+a = a + 3; //assigns the sum of a+3 to a
+console.log(a); //Output a's value: 5
+
