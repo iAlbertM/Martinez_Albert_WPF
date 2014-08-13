b261c1429110dedd0e71308218960dd60a01067f
Albert Martinez <albertm@fullsail.edu>
2014-08-13 10:27:52 -0700

    added Assignment Operators code from screencast

--- Expressions/assignmentOperators/js/script.js
+++ Expressions/assignmentOperators/js/script.js
@@ -1,2 +1,13 @@
+// Expressions - Assignment Operators
 
+var a = 3;
+a + 4;
+console.log(a);//does not change value of a. only refers to original value of a
 
+a = a + 4;// a = 3 + 4
+console.log(a);
+
+//write less do more
+a += 4;//a = a + 4 / a = 7 + 4
+//can use +=, -=, /=. *= 
+console.log(a);
