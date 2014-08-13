c88be0636ceb6870b3ca0a964f4573f8d5f3fc2a
Albert Martinez <albertm@fullsail.edu>
2014-08-13 09:25:54 -0700

    added Concatenating Strings code from screen cast

--- Expressions/concatenatingStrings/js/script.js
+++ Expressions/concatenatingStrings/js/script.js
@@ -1,2 +1,15 @@
+//Expressions - Concatenating Strings
 
+var firstName = "Albert";
+var lastName = "Martinez";
+var fullName = firstName + " " + lastName;
 
+console.log(fullName);
+
+var a = "6";
+var b = "7";
+var result = a + b;
+console.log(result);
+
+//strings can only be combined in concatenation
+//numbers can be manipulated using arithmetic operators
