f8ef194d6a434750d24f8db979e04b4b21e71667
Albert Martinez <albertm@fullsail.edu>
2014-08-13 08:57:39 -0700

    added Order of Operations code from screencast

--- Expressions/orderOfOperations/js/script.js
+++ Expressions/orderOfOperations/js/script.js
@@ -1,2 +1,17 @@
+//Expressions - PEMDAS - Order of Operations
+
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 60;
+var quiz4 = 80;
+
+//average: sum of items divided by # of items
+var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
+console.log(average);
+
+var length = 7;
+var width = 6;
+var perimeter = length * 2 + width * 2;
+console.log(perimeter);
 
 
