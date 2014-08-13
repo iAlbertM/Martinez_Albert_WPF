011baa24e03f6f834e5f8790656f9dee8014cc1f
Albert Martinez <albertm@fullsail.edu>
2014-08-13 01:51:04 -0700

    uploaded Arrays screen cast code notes
    created arrays "follow along" notes

---
+++ ValuesandVariables/Arrays/js/script.js
@@ -0,0 +1,16 @@
+//Arrays
+
+//0-base index
+var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo" ];
+
+//Array Access Notation
+console.log(muppetNames[2]);
+
+// change index & add to index by entering in 
+muppetNames[1] = "Rizzo";
+// any index # after the last listed item in the Array
+muppetNames[8] = "Bert";
+
+// verify what is in a specific index
+console.log(muppetNames[3]);
+
--- ValuesandVariables/stringAndBoolean/js/script.js
+++ ValuesandVariables/stringAndBoolean/js/script.js
@@ -1,11 +1,9 @@
-// Number variable
 var age = 38;
-
+ 
 //Boolean variable
 var isStudent = false;
 
-var firstName = "Kermit";
-
 // var phrase = 'I don\'t know. You\'ll have to speak with the professor.';
+var phrase = 'I don\'t know.\n You\'ll have to speak with the professor.';
 // Do now have to use " \ " if you begin string with "" and string contains " ' "
-var phrase = "I don't know.\n You'll have to speak with the professor.";
+var phrase2 = "I don't know.\n You'll have to speak with the professor.";
