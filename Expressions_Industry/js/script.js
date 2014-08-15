9625d5eb5c195a787da860ea7eadd2c13ef9fdfa
Albert Martinez <albertm@fullsail.edu>
2014-08-14 20:36:27 -0700

    Created problem for industry assignment

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -3,7 +3,8 @@
 //Expressions_Industry
 
 // Problem: Create a base rate of pay for a Freelancer in Web Design and Developement.
-// Givens: To calculate this you need: (target salary + Associated Costs) / Billable hours
+
+
 
 
 //Results Variable:
