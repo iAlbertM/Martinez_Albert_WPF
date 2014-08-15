e93334c82b6429a240bd213204b94325b742f98d
Albert Martinez <albertm@fullsail.edu>
2014-08-14 20:38:06 -0700

    created next variable for formula

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -5,7 +5,7 @@
 // Problem: Create a base rate of pay for a Freelancer in Web Design and Developement.
 // Givens: To calculate this you need: (target salary + Associated Costs) / Billable hours
 var targetSalary;
-
+var associatedCosts;
 
 
 
