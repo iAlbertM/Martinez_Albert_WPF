492392d8172bf8ff2e16e5cad50c9066ff242630
Albert Martinez <albertm@fullsail.edu>
2014-08-14 20:10:14 -0700

    defined 2nd variable

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -7,3 +7,4 @@ var startTime;
 var endTime;
 var tasks;
 startTime = prompt("In order to accurately create a schedule that fits your schedule,\nPlease state what time you generally start your day?");
+endTime = prompt("What is your planned end time?\nPlease include am/pm in your goal time");
