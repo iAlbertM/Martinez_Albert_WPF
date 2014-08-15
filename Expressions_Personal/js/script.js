673b347dc89726a0f60861b0649c19403a1f93b2
Albert Martinez <albertm@fullsail.edu>
2014-08-14 21:06:48 -0700

    formatted printed output

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -7,11 +7,11 @@ var startTime;
 var endTime;
 var tasks;
 startTime = prompt("In order to accurately create a schedule that fits your schedule,\nPlease state what time you generally start your day?");
-endTime = prompt("What is your planned end time?\nPlease include am/pm in your goal time");
+endTime = parseInt(prompt("What is your planned end time?\nPlease do not include am/pm in your goal time"));
 tasks = prompt("How many tasks do you plan on generally completing on a daily basis?");
 
 // formula for dividing up time evenly according to number of daily tasks: start time - end time / # of tasks
 var schedule = startTime - endTime / tasks;
 
 // Print out allotted time for each task
-console.log("In order to meet your goal time of " + endTime + ", each of your " + tasks + " should take approximately " + schedule + "hours.");
+console.log("In order to meet your goal time of " + endTime + ", each of your " + tasks + " tasks should take approximately " + schedule + " hours.");
