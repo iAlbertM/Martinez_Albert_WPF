3f283750307b8ecfda9eba05b295c993602f243a
Albert Martinez <albertm@fullsail.edu>
2014-08-16 00:12:52 -0700

    linked js file to index.html

---
+++ Screencasts/Conditionals/js/script.js
@@ -0,0 +1,10 @@
+// Conditional Logic
+
+var oldEnough = true;
+
+// if the child is old enough, print to the console "You can ride the rollercoaster!"
+if(oldEnough) {
+	// code performed if condition is true
+	console.log("You can ride the rollercoaster!");
+}
+console.log("What comes next");
