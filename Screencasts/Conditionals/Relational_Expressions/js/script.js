3020652c260b8fb44e0d0b9d758d9010e77d5939
Albert Martinez <albertm@fullsail.edu>
2014-08-18 15:39:33 -0700

    added if else statement code notes

--- Screencasts/Conditionals/Relational_Expressions/js/script.js
+++ Screencasts/Conditionals/Relational_Expressions/js/script.js
@@ -2,10 +2,14 @@
 
 var kidHeight = 40;
 var minHeight = 48;
+var sneakerLift = 2;
 
 // if the child is old enough, print to the console "you can ride!"
 // if the kid is over 48 inches in hight
 if(kidHeight > minHeight){
 	//code performed if condition is true
 	console.log("You can ride the coaster!");
+} else {
+	// code performed if condition is false
+	console.log("Sorry kid, you've got some growing to do first!");
 }
