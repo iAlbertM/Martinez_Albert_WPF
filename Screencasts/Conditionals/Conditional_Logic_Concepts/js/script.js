bc82aceb2808391e8c00ee5e2b97dcbf616bdd7a
Albert Martinez <albertm@fullsail.edu>
2014-08-18 14:47:53 -0700

    Created proper file structure

---
+++ Screencasts/Conditionals/Conditional_Logic_Concepts/js/script.js
@@ -0,0 +1,18 @@
+//Conditional Statements
+
+sunny = true; 
+
+if(sunny){
+	goToTheBeach();
+} else {
+	goToTheMovies();
+};
+
+var temp = 85
+if(temp > 70) {
+	goToTheBeach();
+}
+
+if(temp == 75){
+	goToTheBeach();
+}
