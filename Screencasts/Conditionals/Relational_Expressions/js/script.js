b2c47198ed41eb5dd83923856827724912d67837
Albert Martinez <albertm@fullsail.edu>
2014-08-20 11:19:39 -0700

    cleaned up notes
    fixed spelling and grammatical errors, formatted code, tidied up

--- Screencasts/Conditionals/Relational_Expressions/js/script.js
+++ Screencasts/Conditionals/Relational_Expressions/js/script.js
@@ -4,15 +4,15 @@ var kidHeight = 40;
 var minHeight = 48;
 var sneakerLift = 2;
 var wParentHeight = 48; // the height of the kid with the parent
-// if the child is old enough, print to the console "you can ride!"
-// if the kid is over 48 inches in hight
-if(kidHeight > minHeight){
-	//you can ride
+
+// if the kid is over 48 inches in height
+if(kidHeight >= minHeight){
+	// you can ride
 	console.log("You can ride the coaster!");
-} else if(kidHeight > wParentHeight){
-	// you can ride with a parent present
+} else if(wParentHeight > minHeight) {
+	// you can ride only with a parent present
 	console.log("You can ride, but only with a parent present.");
 } else{
-	//sorry you growing to do
+	// sorry you've got some growing to do
 	console.log("Sorry kid, you've got some growing to do first!");
 }
