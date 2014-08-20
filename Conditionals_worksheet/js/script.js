98b32bbd7b5c7bab1671f600bb2f4b45d3816f77
Albert Martinez <albertm@fullsail.edu>
2014-08-20 05:41:43 -0700

    defined variables in Group 2 code

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -7,16 +7,13 @@
 
 // Problem: Temperature Converter. Create a program that converts a temperature in degrees Fahrenheit or Celsius entered by the user, which will then convert it into the other (i.e., F to C or C to F) Celsius ("C") or Fahrenheit ("F")
 // Givens:
-// Store the initial temperature, entered by the user, in a variable named: degrees
-var degrees = Number(prompt("Please enter any temperature in \xB0Celsius or \xB0Fahrenheit"));
-
-// Determine the output unit and store the value of the prompt() in a variable named: unit
-var unit = prompt("Enter the Unit you want to convert too:\n'F' for Fahrenheit or 'C' for Celsius");
+var degrees = Number(prompt("Please enter any temperature in \xB0Celsius or \xB0Fahrenheit")); // Prompt user for initial temperature and store in a variable named: degrees
+var unit = prompt("Enter the Unit you want to convert too:\n'F' for Fahrenheit or 'C' for Celsius"); // Prompt user for the output unit and store in a variable named: unit
 var converted;
 
 // If the value of unit is equal to "F"
 if(unit.toUpperCase() == "F") {
-	// Convert unit into degrees Fahrenheit and store in a variable named: converted
+	// Then convert unit into degrees Fahrenheit and store in a variable named: converted
 	converted = degrees * 9 / 5 + 32;
 	// Print to the console: first the converted temperature | the originating temperature
 	console.log("The Temperature is " + converted + "\xB0F | " + degrees + "\xB0C");
@@ -37,4 +34,18 @@ else if(unit.toUpperCase() == "C") {
 // Group 2: Multiple Results
 // Check the Login
 
-//Problem: Provide user login and password validation. Including pop-up error messages when incorrect or invalid entries are submitted.
+// Problem: Provide user login and password validation. Including pop-up error messages when incorrect or invalid entries are submitted.
+// Givens:
+var goodName = prompt("To create a login:\nPlease enter a username"); // Prompt user for sername. Store in variable: userName
+var goodPass = prompt("For your security,\nPlease enter a password"); // Prompt user for a password. Store in variable: userPass
+// Prompt user for existing userName and userPass
+var userName = prompt("Please enter your username");
+var userPass = prompt("Please enter your password");
+
+// Check if input matches userName and userPass
+
+
+
+// Print to the console: "User not found. Try again." if userName incorrect
+
+// Print to the console: “Password does not match our records.”  if userName matches but userPass does not
