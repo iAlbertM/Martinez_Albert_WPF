ce6cde7cae88130dfebf32cbbbe3253cae88bdfa
Albert Martinez <albertm@fullsail.edu>
2014-08-20 08:52:48 -0700

    started Group 3 Tire Pressure 1
    Defined problem and variables

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -8,21 +8,21 @@
 
 // Problem: Temperature Converter. Create a program that converts a temperature in degrees Fahrenheit or Celsius entered by the user, which will then convert it into the other (i.e., F to C or C to F) Celsius ("C") or Fahrenheit ("F")
 // Givens:
-var degrees = Number(prompt("Please enter any temperature in \xB0Celsius or \xB0Fahrenheit")); // Prompt user for initial temperature and store in a variable named: degrees
-var unit = prompt("Enter the Unit you want to convert too:\n'F' for Fahrenheit or 'C' for Celsius"); // Prompt user for the output unit and store in a variable named: unit
+var degrees = Number(prompt("Please enter a temperature you would like to convert into either \xB0Celsius or \xB0Fahrenheit")); // Prompt user for initial temperature and store in a variable named: degrees
+var unit = prompt("Enter the Unit you would like to convert the temperature to:\nType 'F' for Fahrenheit - or - 'C' for Celsius"); // Prompt user for the output unit and store in a variable named: unit
 var converted;
 
 // If the value of unit is equal to "F"
 if(unit.toUpperCase() == "F") {
 	converted = degrees * 9 / 5 + 32; // Then convert unit into degrees Fahrenheit and store in a variable named: converted
-	console.log("The Temperature is " + converted + "\xB0F | " + degrees + "\xB0C"); // Print to the console: first the converted temperature | the originating temperature
+	console.log("Your initial " + degrees + "\xB0C temperature is " + converted + "\xB0F"); // Print to the console: first the converted temperature | the originating temperature
 }
 // If the value of unit is equal to "C"
 else if(unit.toUpperCase() == "C") {
 	converted = 5 / 9 * (degrees - 32); // Convert unit into degrees Celsius and store in a variable named: converted
-	console.log("The Temperature is " + converted + "\xB0C | " + degrees + "\xB0F"); // Print to the console: first the converted temperature | the originating temperature
+	console.log("Your initial " + degrees + "\xB0F temperature is " + converted + "\xB0C"); // Print to the console: first the converted temperature | the originating temperature
 } else {
-	console.log("I'm sorry but that wasn't a valid input. You need to enter a letter 'C' for Celsius or 'F' for Fahrenheit"); // Otherwise print to the console: "No valin input was found"
+	console.log("Invalid entry. Go ahead and try again by refreshing the page with: Ctrl + R (Windows) or Cmd + R (Mac)"); // Otherwise print to the console: "No valin input was found"
 }
 
 
@@ -50,4 +50,87 @@ else if (!(corUser) && corPass) {
 else if (corUser && !(corPass)) {
 	console.log("Password does not match our records"); // Print to the console: “Password does not match our records.”
 }
+
+
+// Group 3: Multiple Conditions
+// Tire Pressure 1
+
+// Problem: Tires need correct pressure to pass spec. Front tires must have equal tire pressure, same for the rear tires. The front and rear pressures do not need to match each other.
+
+// Givens:
+var tireFL;
+var tireFR;
+var tireRL;
+var tireRR;
+var tiresPSI = []
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
