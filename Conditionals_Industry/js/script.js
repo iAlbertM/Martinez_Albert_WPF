299977148bb431be62df25353bf00b9f510c916c
Albert Martinez <albertm@fullsail.edu>
2014-08-21 21:55:11 -0700

    Redefined and re-established formula and var

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -2,21 +2,23 @@
 // 08/21/2014
 // Conditionals Industry
 
-// Problem: Create an estimate to provide a rough estimate on the cost of a project based on a weekly average hours worked and multiplied by the rate of pay
-
-// Givens:
-var workHrs = prompt("To determine estimate for quote, How many hours did you work on the project this week (whole numbers please)?");
+// Problem: Create an program to quickly provide a rough estimate of your weekly pay as a Desiner and/or Developer
+var workHrs = prompt("Weekly Hours\nHow many hours did you work on the project this week?");
  if (workHrs == "") { // validation for the user input
- 	prompt("You may have forgotten to fill that last field there. Happens to the best of us when we're excited! Try again.");
+ 	prompt("You may have forgotten to fill that last field there.\nHappens to the best of us when we're excited! Try again.");
  }
  if (workHrs == 0) { //checking to see if user worked at all
  	console.log("No work equals no pay. Sorry! Try harder next time!")
  }
-var ratePay = prompt("What is your rate of Pay with these types of projects?"); // Needed for formula for client quote
- if (ratePay == "") {// checking for empty string --validation
- 	prompt("You may have forgotten to fill that last field there. Happens to the best of us when we're excited! Try again.");
+var hourlyRate = prompt("What is your base rate on these project?"); // Needed for formula for client quote
+ if (hourlyRate == "") {// checking for empty string --validation 
+	prompt("You may have forgotten to fill that last field there.\nHappens to the best of us when we're excited! Try again.");
  }
-var wkProgress = workHrs * ratePay; // calculating the project quote
+var duration = prompt("In terms of weeks,\nHow long will it take you to finish this type of project?");
+ if (duration == "") {// checking for empty string --validation
+ 	prompt("You may have forgotten to fill that last field there.\nHappens to the best of us when we're excited! Try again.");
+ }
+var wkProgress = workHrs * hourlyRate / duration; // calculating the project weekly prie rate for the client's project
 
-// Print to the console the results of the user input and the end rough estimate it equates to. 
-console.log("Based on your " + workHrs + " hours worked this week at your current rate of pay, $" + ratePay + ", you can quote the client: $" + wkProgress + "!");
+// Print to the console the results of the user input and the end rough estimate it equates to.
+console.log("Based on your " + workHrs + " hours worked this week\nat your current rate of pay, $" + hourlyRate + ",\n you can quote the client: $" + wkProgress + "!");
