c27a3b7813d31b4dc713d6e153da7a04b57d2b05
Albert Martinez <albertm@fullsail.edu>
2014-08-23 03:54:28 -0700

    + notes on function calls

---
+++ Screencasts/Functions/Function_Calls.js
@@ -0,0 +1,18 @@
+// Albert Martinez
+// web programming fundamentals
+// Function - Calls
+
+function outputMsg(){
+	console.log("Hello World");
+}
+// Function Definition
+function calcArea() {
+	var width = 20;
+	var height = 30;
+	var area = width * height;
+	console.log(area);
+}
+// calling or invoking a function is the only way to run a function
+// Function Call()
+calcArea();
+
