e4b20f03fa8ee439a03e2f1ffd8eefdcdc5767fe
Albert Martinez <albertm@fullsail.edu>
2014-08-23 08:06:03 -0700

    updated notes and titles

--- Screencasts/Functions/Loops.js
+++ Screencasts/Functions/Loops.js
@@ -5,10 +5,10 @@
 
 // While Loops
 console.log("---------- while loops ----------");
-var b = 50; // Sets up the index
+var a = 50; // Sets up the index
 
-while (b > 0){ // Checks the condition
-	console.log(b + "bongs on the wall");
+while (a > 0){ // Checks the condition
+	console.log(a + "bottles of butter beer on the wall");
 	b--; // Increments or Decrements the index
 }
 
@@ -20,7 +20,7 @@ console.log("---------- do...while loops ----------");
 var c = 10;
 
 do{ // Code block will always run once before the loops first iteration
-	console.log(c + " Kegs on the wall")
+	console.log(c + " bottles of butter beer on the wall")
 	c--;
 }
 while(c > 0); 
@@ -30,5 +30,5 @@ while(c > 0);
 console.log("---------- for loops ----------");
 
 for (var i = 10; i > 0; i--){
-	console.log(i + " kegs on the wall")
+	console.log(i + " bottles of butter beer on the wall")
 }
