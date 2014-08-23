35d41403684b772b59cb1b52f9fc133afc12bb7d
Albert Martinez <albertm@fullsail.edu>
2014-08-23 04:00:25 -0700

    + notes on returning values in functions

--- Screencasts/Functions/Returning.js
+++ Screencasts/Functions/Returning.js
@@ -1,30 +1,14 @@
 // Albert Martinez
 // web programming fundamentals
-// Functions - Variable Scope
+// Functions - Returning Values
 
-// funcName(argument1, argument2);
-// function funcName(parameter1, parameter2) {
-	//code to run}
+var total = calcArea(30, 20); // whatever the function returns, store into this variable total
 
-// calcArea(30, 20); // arguments - info stored in our parameters
-
-// function calcArea(w, h) { // storage bin the info gets stored in w=30, h=20
-// 	var area = w * h;
-// 	console.log(area);}
-
-function dogYears(){
-	var age = 4;
-	var dogYears = age * 7;
-	console.log("Sparky is " + dogYears + " years old.")
+function calcArea(w, h) { // storage bin the info gets stored in w=30, h=20
+	var area = w * h;
+	return area; // spitting the info out
 }
-console.log(dogYears());
 
-function dogYears(age){ // parameters: "()" act as the storage container for the information such as the value of the age variable
-	var dogYears = age * 7;
-	console.log("Sparky is " + dogYears + " years old.")
-}
-dogYears(4); // passing the value 4 to age
+console.log(total);
 
-var age1 = 4;
-dogYears(age1); // arguments will always be found in the function call or the function invocation
-
+// functions that are returning information must be stored into a variable
