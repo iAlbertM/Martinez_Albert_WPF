b6d6b5d45344375b9632ea9f8e0ea52eda2565ad
Albert Martinez <albertm@fullsail.edu>
2014-08-23 04:59:08 -0700

    +notes for loops & updated test.html for testing

---
+++ Screencasts/Functions/While_Loops.js
@@ -0,0 +1,30 @@
+// Albert Martinez
+// web programming fundamentals
+// Loops
+
+
+// While Loops
+console.log("---------- while loops ----------");
+var b = 50; // Sets up the index
+
+while (b > 0){ // Checks the condition
+	console.log(b + "bongs on the wall");
+	b--; // Increments or Decrements the index
+}
+
+
+// Loops - Do WHile Loops
+console.log("---------- do...while loops ----------");
+
+
+var c = 10;
+
+do{ // Code block will always run once before the loops first iteration
+	console.log(c + " Kegs on the wall")
+	c--;
+}
+while(c > 0); 
+
+
+
+
