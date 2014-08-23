778de54855f93a4da8696ae9d0916c848fc56989
Albert Martinez <albertm@fullsail.edu>
2014-08-23 03:55:08 -0700

    + notes for basic structure

---
+++ Screencasts/Functions/Basic_structure.js
@@ -0,0 +1,14 @@
+// Albert Martinez
+// web programming fundamentals
+// Functions - Basic Structure
+
+function outputMsg(){
+	console.log("Hello World");
+}
+
+function calcArea() {
+	var width = 20;
+	var height = 30;
+	var area = width * height;
+	console.log(area);
+}
