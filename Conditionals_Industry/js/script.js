0e3ba89ce8cec91bf8add8fc648fd38f6dd8b1d3
Albert Martinez <albertm@fullsail.edu>
2014-08-22 20:11:41 -0700

    rewrote output to the console

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -5,7 +5,7 @@
 // Problem: Create an program to quickly provide a rough estimate of your weekly pay as a Desiner and/or Developer
 
 // Givens
-var workHrs = prompt("Weekly Hours\nHow many hours did you work on the project this week?");
+var workHrs = prompt("Weekly Hours\nHow many hours do you plan to spend working on the project each week?"); // Gathering estimated hours that will be billable
  if (workHrs == "") { // validation for the user input
  	prompt("You may have forgotten to fill that last field there.\nHappens to the best of us when we're excited! Try again.");
  }
@@ -23,4 +23,4 @@ var duration = prompt("In terms of weeks,\nHow long will it take you to finish t
 var wkProgress = workHrs * hourlyRate / duration; // calculating the project weekly prie rate for the client's project
 
 // Print to the console the results of the user input and the end rough estimate it equates to.
-console.log("Based on your " + workHrs + " hours worked this week\nat your current rate of pay, $" + hourlyRate + ",\n you can quote the client: $" + wkProgress + "!");
+console.log("Based on your estimated " + workHrs + " hours a week dedicated to the project,\nMultiplied by your current hourly rate of $" + hourlyRate + ",\n you can quote the client: $" + wkProgress + " for the project");
