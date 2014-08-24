352edfd6068857190cfb3102af9749b0f56f99ce
Albert Martinez <albertm@fullsail.edu>
2014-08-24 12:38:38 -0700

    added stung  problem and solution

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -2,7 +2,7 @@
 // 08/23/14
 // Functions_worksheet
 
-// Circumference ---------- *
+// CIRCUMFERENCE ---------- *
 
 // Problem: Calculate the circumference of a circle
 // Formula: Circumference (C) = 2π(PI)r(radius)
@@ -15,4 +15,20 @@ console.log("The circumference of the circle is " + circumference);
 
 function calcCirc(r){				// Defining the calcCirc() wand establishing a parameter called: radius
 	return 2 * Math.PI * r;			// The code block that will execute when called/invoked. The output will be returned to the result variable: circumference
+}
+
+
+// STUNG ---------- *
+
+// Problem: Calculate how many bee stings it takes to kill an animal in a function
+
+// Givens
+// To kill an animal =  8.666666667 bee stings * 1.0lb
+var victimWt = 150; // Victims wight in lbs.
+var fatalStings = beeSting(victimWt);
+
+console.log("It takes " + fatalStings + " to kill this animal.");
+
+function beeSting(wt){
+	return 8.666666667 * wt;
 }
