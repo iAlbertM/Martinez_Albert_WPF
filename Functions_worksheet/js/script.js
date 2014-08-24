455c3d9e4c00f453c5e448173936c8f510ac4193
Albert Martinez <albertm@fullsail.edu>
2014-08-24 00:29:49 -0700

    + code: defined problem. not yet tested

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -7,3 +7,10 @@
 // Problem: Calculate the circumference of a circle
 // Formula: C = 2πr
 // cirumference = 2 * π * radius
+var radius = 2;
+var circumference = calcCirc(radius);
+
+console.log("The circumference of the circle is " + circumference);
+function calcCirc(r) {
+	return 2 * π * r;
+}
