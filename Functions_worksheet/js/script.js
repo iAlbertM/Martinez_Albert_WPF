79e563e6e8bafb7ca0df4671f3c6b5668b0ae0f9
Albert Martinez <albertm@fullsail.edu>
2014-08-24 00:28:06 -0700

    setup Functions_worksheet structure in repo

---
+++ Functions_worksheet/js/script.js
@@ -0,0 +1,9 @@
+// Albert Martinez
+// 08/23/14
+// Functions_worksheet
+
+// Circumference ---------- *
+
+// Problem: Calculate the circumference of a circle
+// Formula: C = 2πr
+// cirumference = 2 * π * radius
