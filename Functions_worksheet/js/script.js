8d0bfbb4d35a73033395be974c6002db59867596
Albert Martinez <albertm@fullsail.edu>
2014-08-24 07:16:03 -0700

    +finished problem 1 from functions worksheet

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -5,12 +5,14 @@
 // Circumference ---------- *
 
 // Problem: Calculate the circumference of a circle
-// Formula: C = 2πr
-// cirumference = 2 * π * radius
-var radius = 2;
-var circumference = calcCirc(radius);
+// Formula: Circumference (C) = 2π(PI)r(radius)
+
+// Givens --
+var radius = 2;		// Assigning the radius variable a value of 2
+var circumference = calcCirc(radius);	// Declaring the result variable / Set the result variable to the result from passing the radius argument to the calcCirc function
 
 console.log("The circumference of the circle is " + circumference);
-function calcCirc(r) {
-	return 2 * π * r;
+
+function calcCirc(r){				// Defining the calcCirc() wand establishing a parameter called: radius
+	return 2 * Math.PI * r;			// The code block that will execute when called/invoked. The output will be returned to the result variable: circumference
 }
