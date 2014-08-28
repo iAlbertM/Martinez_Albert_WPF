86fbbb7256ddb458328344f2abd3709c9b79e98d
Albert Martinez <albertm@fullsail.edu>
2014-08-28 10:19:31 -0700

    + files for functions assignment

---
+++ Functions_Industry/js/script.js
@@ -0,0 +1,5 @@
+// Albert Martinez
+// 08/28/2014
+// Functions - Industry
+
+
---
+++ Functions_Wacky/js/script.js
@@ -0,0 +1,7 @@
+// Albert Martinez
+// 08/28/2014
+// Functions_Wacky
+
+// Problem: Flauston Paradise Galaxy Cruise Lines is the universes first luxury spacecraft island getaway. To kick off its grand opening it is offering amazing package deals that are literally out of this world.
+// Create a program that calculates the vacation packages and their deal prices according to the number of nights a guest is requesting. There is also an option (additional cost) to see a mystifying stellar opera concert by the rare singing blue diva herself.
+
