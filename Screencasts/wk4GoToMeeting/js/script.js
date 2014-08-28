71c3b37b2d8d2b966c50240a931d2e53b0dff70d
Albert Martinez <albertm@fullsail.edu>
2014-08-28 10:19:51 -0700

    +files for goto meeting wk 4

---
+++ Screencasts/wk4GoToMeeting/js/script.js
@@ -0,0 +1,7 @@
+// Albert Martinez
+// WK 4 Video Lecture Notes
+
+// Create a function that takes a min and max number and returns a random number between them
+
+// PROMPT USER FOR MIN NUM
+var min = prompt("Let's get a random number between two numbers\nPlease enter a min number:");
