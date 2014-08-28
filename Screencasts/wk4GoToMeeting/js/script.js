291d78531a6cc4a6ba52c1c0bd2218973eef9150
Albert Martinez <albertm@fullsail.edu>
2014-08-28 10:20:49 -0700

    validated user prompts for min variable

--- Screencasts/wk4GoToMeeting/js/script.js
+++ Screencasts/wk4GoToMeeting/js/script.js
@@ -5,3 +5,10 @@
 
 // PROMPT USER FOR MIN NUM
 var min = prompt("Let's get a random number between two numbers\nPlease enter a min number:");
+
+// validate the user prompts
+// check if user leaves prompts blank or if input isNaN()
+while (min === "" || isNaN(min)) {
+	// re-prompt user while either condition is true
+	min = prompt("Do not leave fields empty and enter only numbers!\nPlease enter a min number:")
+}
