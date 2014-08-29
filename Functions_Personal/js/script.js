d015684b376f1c43281e1d12ccd7c75dfce29672
Albert Martinez <albertm@fullsail.edu>
2014-08-28 20:17:35 -0700

    defined a program that served as a solution
    to my problem

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -2,7 +2,7 @@
 // 08/28/2014
 // Functions_Personal
 
-// Create a function that returns the status of your blood pressure reading = Normal, High Normal, High, Very High, Seek Medical Attention
+// Create a program that returns the status of your blood pressure reading = Normal, High Normal, High, Very High, Seek Medical Attention
 // prompt user for top/ first/ larger(normally) number also called the systolic blood pressure number
 var sysNum = prompt("Time to check your blood pressure status and check your heart status\nPlease provide your systolic blood pressure number (the top/first/higher number of the two--generally speaking))";
 
@@ -20,12 +20,19 @@ while (diaNum === "" || isNaN(diaNum)) {
 	// re prompt user for information if invalid or no input was recieved
 	diaNum = prompt("Do not leave any fields blank and enter numbers only!\nPlease provide your diastolic blood pressure number");	
 }
-// capture results in variable named bloodPressure
-
+var normal = "Your blood pressure is Normal";
+var highNormal = "You should take caution with your blood pressure and see a doctor about changing into healthier life habits and choices, you may have prehypertension");
+var highBP1 = "You should speak with your doctor because you have symptons of High Blood Pressure stage 1";
+var highBP2 = "Seek Medical attention promptly, you are having symptons of Very High Blood Pressure stage 2"
 // create categories for the "if...then" statements for the varying 5 levels of blood pressure results
 if( sys < 120 && dia < 80) {
-	console.log("Your blood pressure is Normal");
-}
-
-
+	console.log(normal);
+} else if ( sys > 120 && sys < 139 || dia > 80 && dia < 89){
+	console.log(highNormal);
+} else if ( sys > 140 && sys < 159 || dia > 90 && dia < 99){
+	console.log(highBP1);
+} else if ( sys >= 160 || dia >= 100){
+	console.log(highBP2);
+} else {
+	console.log("For all other results please consult your physician or immediate medical attention!");
 }
