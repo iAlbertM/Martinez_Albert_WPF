728a0f42957fec3d5d1c5bfe3d4e06302f7d6f48
Albert Martinez <albertm@fullsail.edu>
2014-08-28 21:52:41 -0700

    added references to document.

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,7 +1,8 @@
 // Albert Martinez
 // 08/28/2014
 // Functions_Personal
-
+/* REFERENCES: The American Heart Association (2014) Understanding Blood Pressure Readings. Retrieved from: 
+	http://www.heart.org/HEARTORG/Conditions/HighBloodPressure/AboutHighBloodPressure/Understanding-Blood-Pressure-Readings_UCM_301764_Article.jsp*/
 // Create a program that returns the status of your blood pressure reading = Normal, High Normal, High, Very High, Seek Medical Attention
 // prompt user for top/ first/ larger(normally) number also called the systolic blood pressure number
 var sysNum = prompt("Time to check your blood pressure status\nPlease provide your systolic blood pressure number");
@@ -20,11 +21,14 @@ while (diaNum === "" || isNaN(diaNum)) {
 	// re prompt user for information if invalid or no input was recieved
 	diaNum = prompt("Do not leave any fields blank and enter numbers only!\nPlease provide your diastolic blood pressure number");	
 }
+
+// create categories for the "if...then" statements for the varying 5 levels of blood pressure results 
 var normal = "Your blood pressure is Normal";
 var highNormal = "You should take caution with your blood pressure and see a doctor about changing into healthier life habits and choices, you may have prehypertension";
 var highBP1 = "You should speak with your doctor because you have symptons of High Blood Pressure stage 1";
 var highBP2 = "Seek Medical attention promptly, you are having symptons of Very High Blood Pressure stage 2"
-// create categories for the "if...then" statements for the varying 5 levels of blood pressure results
+
+// use conditionals to check the several outcomes in each category of blood pressure readings(American Heart Association, 2014)
 if( sysNum <= 120 && diaNum <= 80) {
 	console.log(normal);
 }
@@ -34,6 +38,6 @@ if ( sysNum > 120 && sysNum < 139 || diaNum > 80 && diaNum < 89){
 if ( sysNum > 140 && sysNum < 159 || diaNum > 90 && diaNum < 99){
 	console.log(highBP1);
 }
-if ( sysNum >= 160 || diaNum >= 100){
+else if ( sysNum >= 160 || diaNum >= 100){
 	console.log(highBP2);
 }
