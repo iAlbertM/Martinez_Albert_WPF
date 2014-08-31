7b8aa3cbc8aaa821e7ca1c08a9e8a9e12e111eec
Albert Martinez <albertm@fullsail.edu>
2014-08-30 21:08:05 -0700

    restructured the function call
    rewrite of the reshape() in order for the values to be parsed as
    numerical values instead of string values.

--- testPrograms/js/script.js
+++ testPrograms/js/script.js
@@ -9,19 +9,19 @@ var width = 1920;
 var height = 1080;
 
 // prompt the user for the second elements dimensions
-var testWidth = prompt("To find the differneces in sizes of the objects we need its dimensions\nPlease begin by entering the elements width:");
+var testWidth = parseInt(prompt("To find the differneces in sizes of the objects we need its dimensions\nPlease begin by entering the elements width:"));
 // Validate user prompts: check for blank fields and string values
 while (testWidth === "" || isNaN(testWidth)) {
 	// reprompt the user
-	testWidth = prompt("Please do not leave fields blank and only use numerical values.\nPlease enter a value for elements width:");
+	testWidth = parseInt(prompt("Please do not leave fields blank and only use numerical values.\nPlease enter a value for elements width:"));
 }
 
 
-var testHeight = prompt("Enter its height value:");
+var testHeight = parseInt(prompt("Enter its height value:"));
 // Validate user prompts: check for blank fields and string values
 while (testHeight === "" || isNaN(testHeight)) {
 	// reprompt the user
-	testHeight = prompt("Please do not leave fields blank and only use numerical values.\nPlease enter a value for elements height:");
+	testHeight = parseInt(prompt("Please do not leave fields blank and only use numerical values.\nPlease enter a value for elements height:"));
 }
 
 // CREATE: a function that compares the values of the test dimensions with the desired dimensions and returns the test element with the new dimensions
@@ -33,20 +33,31 @@ function reshape(testW, testH) {
 
 	// determine the excess (-) or needed (+) value to convert the test dimensions into the user defined dimensions
 	if(width < testW) {
-		testW = testW - (testW - width);
+		console.log("Your elements width is greater than the target width.\nThe difference is " + Number(testW - width) + "px");
+		var resize = prompt("Would you like to resize your element?\nPlease enter 'y' for yes or 'n' for no");
+		if (resize.toUppercase() === "Y" || resize.toUppercase === "YES") {
+			testW -= (testW - width);
+		} else {
+			console.log("width check: complete");
+		}
+		
 	} else if (width > testW) {
-		w = width + (width - testW);
+		console.log("Your elements width is less than the target width.\nThe difference is " + Number(width - testW) + "px");
+		testW += (width - testW);
 	} else {
-		console.log("I've got nothing in here");
-	} return w + "px";
+		console.log("I've got no changes in here");
+	} w = Number(testW);
 
 	if(height < testH) {
-		h = testH - (testH - height);
+		console.log("the height difference is " + Number(testH - height) + "px");
+		testH -= (testH - height);
 	} else if (height > testH) {
-		h = height + (height - testH);
+		console.log("the height difference is " + Number(height - testH) + "px");
+		testH += (height - testH);
 	} else {
-		h = testH;
-	} return h + "px"
+		console.log("what! I'm done!");
+	} h = Number(testH);
+	return Number(w) + "px x " + Number(h) + "px";
 }
 
 
